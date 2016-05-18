09a71c6c2a54f6a33c362a04008cdc2bb46258c7
Lascu Mihai <mihai.lascu@rocketmail.com>
2016-05-18 18:18:31 +0300

    yolo

---
+++ labs/demo/__init__.py
@@ -0,0 +1,20 @@
+# not my first app
+
+a = 10
+my_string = "aaa"
+#print a, my_string
+
+#my_dict = {'test': 10,
+#			'abc': [1,2,'test','test2'],
+#			'c': dict(a=10),
+#			'cd': {'a': 10}
+#}
+#print my_dict
+#print my_dict['abc']
+#my_dict['c']='a'
+#print dir(my_dict)
+
+
+#jasdgnsadojgnsdgn
+a=range(10)
+print a[2:-1:2]
